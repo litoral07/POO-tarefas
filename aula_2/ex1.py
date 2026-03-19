772b29cb482aaecab5f4a0c734c75a16fa11cded
Emanuel de Melo Pegado <20251011110019@ifrn.local>
2026-03-19 10:59:46 -0300

    lista 01

---
+++ aula_2/ex1.py
@@ -0,0 +1,25 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+sp = 0
+si = 0
+
+if a % 2 == 0: 
+    sp += a 
+else: 
+    si += a
+if b % 2 == 0:
+     sp += b
+else: 
+    si += b
+if c % 2 == 0:
+     sp += c
+else: 
+    si += c
+if d % 2 == 0:
+     sp += d 
+else:
+    si += d
+print("soma dos pares ", sp)
+print("soma dos impares ", si)
